a7629d854c7c0c5d3f853dd4c4952d052afb7199
Zhangc <legendztz@outlook.com>
2023-05-26 05:54:23 -0400

    Router

---
+++ routerhere/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />}>
+        
+        </Route>
+        <Route path="/new-meetup" element={<NewMeetupPage />}>
+          
+        </Route>
+        <Route path="/favorites" element={<FavoritesPage />}>
+          
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
