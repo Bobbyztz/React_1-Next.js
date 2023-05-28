4556e85ba4499a38815939e0e1e7baaac95954e3
Zhangc <legendztz@outlook.com>
2023-05-28 16:19:59 -0400

    combine card with Meetup

---
+++ routerhere/src/components/ui/Card.js
@@ -0,0 +1,8 @@
+import classes from './Card.module.css'
+
+function Card() {
+    return <div classNmae={classes.card}></div>;
+  }
+  
+  export default Card;
+  
--- routerhere/src/pages/AllMeetups.js
+++ routerhere/src/pages/AllMeetups.js
@@ -1,17 +1,21 @@
+import MeetupList from "../components/meetups/MeetipList";
+
 const DUMMY_DATA = [
   {
     id: "cat",
     title: "First Meetup",
     image:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.guim.co.uk%2Fimg%2Fmedia%2F26392d05302e02f7bf4eb143bb84c8097d09144b%2F446_167_3683_2210%2Fmaster%2F3683.jpg%3Fwidth%3D1200%26quality%3D85%26auto%3Dformat%26fit%3Dmax%26s%3Da52bbe202f57ac0f5ff7f47166906403&tbnid=iqNUK8so7yD7gM&vet=12ahUKEwjco_OJyZb_AhWpGVkFHSTcD48QMygIegUIARD2AQ..i&imgrefurl=https%3A%2F%2Fwww.theguardian.com%2Flifeandstyle%2F2020%2Fsep%2F05%2Fwhat-cats-mean-by-miaow-japans-pet-guru-knows-just-what-your-feline-friend-wants&docid=V4qL_jTRbB2FuM&w=1200&h=720&q=cat&ved=2ahUKEwjco_OJyZb_AhWpGVkFHSTcD48QMygIegUIARD2AQ",
+      "https://en.wikipedia.org/wiki/Cat#/media/File:Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg",
     address: "123 Main St, Some City, Some State, 12345",
+    description: "here is a cat",
   },
   {
     id: "dog",
     title: "Second Meetup",
     image:
-      "https://www.google.com/search?q=dog&tbm=isch&ved=2ahUKEwjco_OJyZb_AhWpGVkFHSTcD48Q2-cCegQIABAA&oq=dog&gs_lcp=CgNpbWcQA1AAWABgAGgAcAB4AIABAIgBAJIBAJgBAKoBC2d3cy13aXotaW1n&sclient=img&ei=UIdyZNy7Iamz5NoPpLi_-Ag&bih=1031&biw=2048#imgrc=hZOZMtGMqallhM",
+      "",
     address: "456 Central Ave, Another City, Another State, 67890",
+    description: "here is a dog",
   },
 ];
 
@@ -19,9 +23,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul>{DUMMY_DATA.map((meetup) => {
-        return <li key={meetup.id}>{meetup.id}</li>
-      })}</ul>
+      <MeetupList meetups={DUMMY_DATA}/>
     </section>
   );
 }
