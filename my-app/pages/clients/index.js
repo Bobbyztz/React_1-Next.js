065e41f57a6286cac15982fea1d0a1447fed8529
Zhangc <legendztz@outlook.com>
2023-06-11 20:35:14 -0400

    Link - generate page dynamicly

--- my-app/pages/clients/index.js
+++ my-app/pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maxlalala" },
+    { id: "manu", name: "Manulululu" },
+  ];
+
   return (
     <div>
-      <h1>hello Clients</h1>
+      <h1>The clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
